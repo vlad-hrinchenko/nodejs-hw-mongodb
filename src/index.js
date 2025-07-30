1506b066cb147aff09e68129d0525ed38e529c14
Vlad Hrinchenko <vladuslavhrinchenko@gmail.com>
2025-07-30 15:55:06 +0300

    fix port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || "contacts-q6x3.onrender.com;"
 
 const bootstrap = async () => {
   await initMongoConnection();
