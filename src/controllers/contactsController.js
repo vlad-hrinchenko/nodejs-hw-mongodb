0a67e82554693a2a15ec89bb6ae3099f82a61565
Vlad Hrinchenko <vladuslavhrinchenko@gmail.com>
2025-08-01 18:44:29 +0300

    add GET with id

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -1,4 +1,4 @@
-import { getAllContacts } from '../services/contacts.js';
+import { getAllContacts, getContactById } from '../services/contacts.js';
 
 export const getContactsController = async (req, res) => {
   try {
@@ -13,6 +13,32 @@ export const getContactsController = async (req, res) => {
     res.status(500).json({
       status: 500,
       message: 'Failed to fetch contacts',
+      error: error.message,
+    });
+  }
+};
+
+export const getContactByIdController = async (req, res) => {
+  try {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      return res.status(404).json({
+        status: 404,
+        message: `Contact with id ${contactId} not found`,
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: 'Contact found successfully!',
+      data: contact,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 500,
+      message: 'Failed to get contact',
       error: error.message,
     });
   }
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,5 +1,9 @@
 import { Contact } from '../models/Contact.js';
 
 export const getAllContacts = async () => {
-  return await Contact.find(); 
+  return await Contact.find();
+};
+
+export const getContactById = async (id) => {
+  return await Contact.findById(id);
 };
