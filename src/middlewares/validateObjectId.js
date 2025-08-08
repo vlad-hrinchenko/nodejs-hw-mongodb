195ddcf7f3d2aaadd7c0a62cae022e2235aa56d0
Vlad Hrinchenko <vladuslavhrinchenko@gmail.com>
2025-08-08 07:01:03 +0300

    fix /id

---
+++ src/middlewares/validateObjectId.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+import createError from 'http-errors';
+
+export const validateObjectId = (req, res, next) => {
+  const { contactId } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(contactId)) {
+    return next(createError(400, 'Invalid contact id'));
+  }
+  next();
+};
