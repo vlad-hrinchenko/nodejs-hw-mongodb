bf872776434e3029cb870a881cfe872d812c3cd6
Vlad Hrinchenko <vladuslavhrinchenko@gmail.com>
2025-08-24 08:31:11 +0300

    add rename

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -2,8 +2,8 @@ import createHttpError from 'http-errors';
 
 import mongoose from 'mongoose';
 
-import { Session }  from '../models/session.js';
-import { User }  from '../models/user.js';
+import { Session }  from '../models/Session.js';
+import { User }  from '../models/User.js';
 
 
 
